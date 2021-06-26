ded2c22ef3f4c7a0dd132861738b6276f590305b
technobaboo <technobaboo@gmail.com>
2021-06-26 17:45:56 -0500

    Initial wlroots addition

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include "stereokit.h"
+#include <wayland-server.h>
+
+extern "C" {
+#include "wlr/render/egl.h"
+#define static
+#include "wlr/render/gles2.h"
+#undef static
+#include <wlr/types/wlr_compositor.h>
+}
 
 using namespace sk;
+
+typedef struct {
+	struct wl_display *wl_display;
+	struct wl_event_loop *wl_event_loop;
+
+	struct wlr_renderer *renderer;
+	struct wlr_compositor *compositor;
+} wlroots_compositor;
+
+wlroots_compositor compositor;
+
+extern EGLDisplay egl_display;
+extern EGLContext egl_context;
 
 int main() {
 	sk_settings_t sk_settings = {};
@@ -9,6 +31,14 @@ int main() {
 	sk_settings.flatscreen_height = 900;
 	if (!sk_init(sk_settings))
 		return 1;
+
+	compositor.wl_display = wl_display_create();
+	compositor.wl_event_loop = wl_display_get_event_loop(compositor.wl_display);
+
+	struct wlr_egl *egl = wlr_egl_from_context(egl_display, egl_context, EGL_PLATFORM_GBM_KHR);
+	compositor.renderer = wlr_gles2_renderer_create(egl);
+
+	compositor.compositor = wlr_compositor_create(compositor.wl_display, compositor.renderer);
 
 	while (sk_step([]() {
 	}));
