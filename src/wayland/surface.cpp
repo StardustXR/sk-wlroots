e8823d05e2a2d2b56e76a998da3de4b300eb446f
technobaboo <technobaboo@gmail.com>
2021-07-02 05:39:09 -0500

    Remove unused rendering code comments

--- src/wayland/surface.cpp
+++ src/wayland/surface.cpp
@@ -27,7 +27,6 @@ Surface::Surface(struct wlr_renderer *renderer, struct wlr_surface *surface) {
 	this->renderer = renderer;
 	this->surface = surface;
 
-//	tex_t fakeTex = tex_create_file("/home/nova/Downloads/image.png");
 	this->surfaceTex = tex_create();
 	this->surfaceTex->type            = tex_type_image_nomips;
 	this->surfaceTex->format          = tex_format_rgba32;
@@ -42,8 +41,6 @@ Surface::Surface(struct wlr_renderer *renderer, struct wlr_surface *surface) {
 	this->surfaceShader = shader_create_file("/run/media/nova/MEDIA/xr/stardust/sk-wlroots/src/shaders/shader_unlit_gamma.sks");
 
 	this->surfaceMat = material_create(this->surfaceShader);
-//	this->surfaceMat = material_copy_id(default_id_material_unlit);
-//	material_set_texture(this->surfaceMat, "diffuse", fakeTex);
 	material_set_transparency(this->surfaceMat, transparency_blend);
 	material_set_texture(this->surfaceMat, "diffuse", this->surfaceTex);
 
@@ -77,20 +74,6 @@ void readWaylandTexture(GLuint tex, GLenum target, GLenum format, uint32_t heigh
 
 	glBindFramebuffer   (GL_FRAMEBUFFER, 0);
 	glDeleteFramebuffers(1, &fbo);
-
-//	FILE *f = fopen("/tmp/image.raw", "wb");
-//	fwrite(data, *size, 1, f);
-//	fclose(f);
-//	exit(1);
-
-//	glDeleteTextures(1, outTex);
-//	glGenTextures(1, outTex);
-//	glBindTexture(GL_TEXTURE_2D, *outTex);
-//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, *data);
-//	glBlendColor(0, 1, 0.75, 1);
-//	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, *data);
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//	free(*data);
 }
 
 bool Surface::updateSurface() {
@@ -100,25 +83,11 @@ bool Surface::updateSurface() {
 		return false;
 	}
 	struct wlr_gles2_texture *eglTexture = (struct wlr_gles2_texture *) surfaceTexture;
-	// if(eglTexture.target != GL_TEXTURE_2D)
-		// return false;
-
-//	size_t size;
-//	void *data = nullptr;
-//	readWaylandTexture(eglTexture->tex, eglTexture->target, GL_RGBA, surfaceTexture->height, surfaceTexture->width, &data, &size, &tex);
-
-//	tex_set_colors(this->surfaceTex, surfaceTexture->width, surfaceTexture->height, data);
-//	free(data);
-//	tex_set_surface(this->surfaceTex, (void *) eglTexture->tex, sk::tex_type_image, GL_RGBA, surfaceTexture->width, surfaceTexture->height, 1);
 
 	this->surfaceTex->tex.width       = surfaceTexture->width;
 	this->surfaceTex->tex.height      = surfaceTexture->height;
-//	glBindTexture(eglTexture->target, eglTexture->tex);
 	this->surfaceTex->tex._texture    = eglTexture->tex;
-//	this->surfaceTex->tex._texture    = tex;
 	this->surfaceTex->tex._target     = eglTexture->target;
-//	this->surfaceTex->tex._target     = GL_TEXTURE_EXTERNAL_OES;
-	// printf("%b\n", eglTexture->target == GL_TEXTURE_EXTERNAL_OES);
 	tex_set_options(this->surfaceTex, tex_sample_point, tex_address_clamp, 1);
 
 	return true;
