fd98002d998767afc860d7988775834d68f7e1a5
technobaboo <technobaboo@gmail.com>
2021-07-03 00:18:30 -0500

    Set xdg surfaces to start maximized and activated

--- src/wayland/xdgsurface.cpp
+++ src/wayland/xdgsurface.cpp
@@ -1,4 +1,5 @@
 #include "xdgsurface.hpp"
+#include "xdg-shell-protocol.h"
 #include <wayland-server-core.h>
 
 XDGSurface::XDGSurface(wlr_renderer *renderer, wlr_xdg_surface *xdg_surface) : Surface(renderer, xdg_surface->surface) {
@@ -8,6 +9,11 @@ XDGSurface::XDGSurface(wlr_renderer *renderer, wlr_xdg_surface *xdg_surface) : S
 		printf("New XDG surface created\n");
 
 	mapped = &xdg_surface->mapped;
+
+	wlr_xdg_toplevel_set_activated(xdg_surface, true);
+	// wlr_xdg_toplevel_set_fullscreen(xdg_surface, true);
+	wlr_xdg_toplevel_set_maximized(xdg_surface, true);
+	// wlr_xdg_toplevel_set_size(xdg_surface, xdg_surface->geometry.width, xdg_surface->geometry.height);
 	// surfaceMappedCallback.callback = std::bind(&Surface::onMapped, this);
 	// wl_signal_add(&xdg_surface->events.map, &surfaceMappedCallback.listener);
 }
